54919d75d85ca057eed428dbd43f1de2a7430d4e
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-06-21 21:00:42 +0200

    Z500: update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,9 +23,9 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-LOCAL_PATH := $(call my-dir)
+ifneq ($(filter Z500,$(TARGET_DEVICE)),)
 
-ifeq ($(TARGET_DEVICE),Z500)
+LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
